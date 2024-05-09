4394ec81dd95dd2aa34d8c962df6b7f28ab9cd51
Altair Almeida <71048705+altair-alms@users.noreply.github.com>
2024-05-09 01:11:21 +0000

    Endpoint   `/oi` & Criação do script `start`

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world!")
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+// criação do endpoint /oi -> Olá, Mundo! 
+app.get('/oi', function (req, res) {
+  res.send('Olá, Mundo!')
+})
+
+app.listen(3000)
